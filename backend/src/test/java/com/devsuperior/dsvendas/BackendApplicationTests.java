53310597b7a26309fcb03efaae947113271c9bb8
Beatriz Vilalta <beatrizvilaltajimenez@gmail.com>
2021-05-04 08:05:37 -0300

    Initial Commit

---
+++ backend/src/test/java/com/devsuperior/dsvendas/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
