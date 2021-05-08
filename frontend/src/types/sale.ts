f7b71c9fc00cb99b44e49c7f7bdd1a2cbecbef90
Beatriz Vilalta <beatrizvilaltajimenez@gmail.com>
2021-05-08 19:59:23 -0300

    DataTable integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,7 +1,30 @@
+import { Seller } from "./seller"
+
+export type Sale = {
+id: number;
+visited: number;
+deals: number;
+amount: number;
+date: string;
+seller: Seller;
+}
+
+export type SalePage = {
+    content?: Sale[];
+    last: boolean;
+    totalPages: number;
+    totalElements: number;
+    first: boolean;
+    size?: number;
+    number: number,
+    numberOfElements?: number;
+    empty?: boolean;
+}
+
 export type SaleSum = {
         sellerName: string;
         sum: number;
-    }
+}
 
 export type SaleSuccess = {
     sellerName: string;
